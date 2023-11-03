c5dd637a976612c46d1a0718a8fcc3301387f4cb
seohyeon <uimi3606@gmail.com>
2023-11-04 02:03:21 +0900

    DESIGN 메인페이지 캐러셀 수정

--- src/components/MainReview.js
+++ src/components/MainReview.js
@@ -9,7 +9,7 @@ function MainReview() {
         <div className="MainReviewInner">
           <div className="MainReviewIntro">
             <h2>여행을 안전하게, 여행자들의 리뷰!</h2>
-            <h5>
+            <h5 className="MainReviewIntroSub">
               이미 여행을 다녀온 회원들의 리뷰와 조언을 통해 앞으로 방문할
               여행지를 미리 대비하세요!
             </h5>
@@ -37,10 +37,10 @@ function Carousel() {
         outsideChevron
         chevronWidth={chevronWidth}
       >
-        <div style={{ height: 200, background: "#EEE" }}>First card</div>
-        <div style={{ height: 200, background: "#EEE" }}>Second card</div>
-        <div style={{ height: 200, background: "#EEE" }}>Third card</div>
-        <div style={{ height: 200, background: "#EEE" }}>Fourth card</div>
+        <div className="CaroueslCard">First card</div>
+        <div className="CaroueslCard">Second card</div>
+        <div className="CaroueslCard">Third card</div>
+        <div className="CaroueslCard">Fourth card</div>
       </ItemsCarousel>
     </div>
   );
